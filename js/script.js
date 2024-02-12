27c6b63860589555c62c143877b6ffba88620638
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2024-02-12 19:34:33 +0100

    Add preventDefault() to submit button for testing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const formInputs = document.querySelectorAll(".form-inputs input");
+const submitBtn = document.querySelector("button[type='submit'");
 
 formInputs.forEach((input) => {
   input.addEventListener("keyup", () => {
     if (input.value.length > 0) input.classList.remove("unfilled");
   });
+});
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 });
