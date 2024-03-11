36c323544ed6e2d032b9745cfa58e0fe1697fb7d
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2024-03-11 13:13:38 +0100

    Add password caption and media queries

--- js/script.js
+++ js/script.js
@@ -3,24 +3,35 @@ const submitBtn = document.querySelector("button[type='submit']");
 const passwordInputs = document.querySelectorAll("input[type='password']")
 const password = document.querySelector("#password");
 const passwordConfirmation = document.querySelector("#password-confirmation");
+const passwordCaption = document.querySelector("#password-caption");
 
 const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,32}$/
 
-formInputs.forEach((input) => {
+formInputs.forEach(input => {
   input.addEventListener("keyup", () => {
     if (input.value.length > 0) input.classList.remove("unfilled");
   });
 });
 
-passwordInputs.forEach((input) => {
+passwordInputs.forEach(input => {
   input.addEventListener("keyup", () => {
-    passwordPattern.test(input.value) ? input.classList.remove("invalid") : input.classList.add("invalid");
+    if (passwordPattern.test(input.value)) {
+      input.classList.remove("invalid");
+      passwordCaption.textContent = null;
+    } else {
+      input.classList.add("invalid");
+      passwordCaption.textContent = "Must contain at least one number, one uppercase and lowercase letter, special character, and 8 to 32 characters";
+    } 
   });
 });
 
 submitBtn.addEventListener('click', e => {
   if (!validatePassword(password, passwordConfirmation)) {
     e.preventDefault();
+    passwordCaption.textContent = "* Passwords do not match";
+    passwordInputs.forEach(input => {
+      input.classList.add("invalid");
+    })
   }
 });
 
